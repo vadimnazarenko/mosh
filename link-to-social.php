b36d40cbb07166662d6b99b35bf8cb92b98c12ee
Your Name <vadiknazarenko@gmail.com>
2019-03-18 20:59:21 +0200

    work on sidebar

---
+++ link-to-social.php
@@ -0,0 +1,82 @@
+<style>
+    .connect-with-us-title{
+        font-family: 'Museo Sans W01', helvetica, arial, sans-serif;
+        font-size: 1.5em;
+        font-weight: 800;
+        color: #1D1B1B;
+        line-height: 1.2em;
+        text-align: left;
+        margin-top: 20px;
+    }
+    .social-icons-parent .social-icons{
+        list-style-type: none;
+        padding-left: 0;
+        margin-top: 10px;
+    }
+    .social-icons-parent ul.social-icons li{
+        float: left;
+    }
+    .social-icons-parent ul.social-icons li a{
+        color: #60656f;
+        line-height: 20px;
+        display: inline-block;
+        border: 0;
+        padding: 0 16px 0 0;
+        font-size: 30px;
+    }
+    .social-icons-parent ul.social-icons li a .fa-twitter{
+        color: #469bfc;
+    }
+    .social-icons-parent ul.social-icons li a .fa-twitter:hover{
+        color: #41495b;
+    }
+
+    .social-icons-parent ul.social-icons li a .fa-facebook-square{
+        color: #3c5a86;
+    }
+    .social-icons-parent ul.social-icons li a .fa-facebook-square:hover{
+        color: #41495b;
+    }
+
+    .social-icons-parent ul.social-icons li a .fa-youtube-play{
+        color: #b1161f;
+    }
+    .social-icons-parent ul.social-icons li a .fa-youtube-play:hover{
+        color: #41495b;
+    }
+
+    .social-icons-parent ul.social-icons li a .fa-linkedin-square{
+        color: #127cb4;
+    }
+    .social-icons-parent ul.social-icons li a .fa-linkedin-square:hover{
+        color: #41495b;
+    }
+</style>
+
+<div class="connect-with-us-title">
+    Connect With Us
+</div>
+<div class="social-icons-parent">
+    <ul class="social-icons">
+        <li class="twitter">
+            <a href="https://twitter.com/moshhamedani" title="Mosh on Twitter" target="_blank">
+                <i class="fa fa-twitter"></i>
+            </a>
+        </li>
+        <li class="facebook">
+            <a href="https://www.facebook.com/programmingwithmosh/" title="Mosh on Facebook" target="_blank">
+                <i class="fa fa-facebook-square"></i>
+            </a>
+        </li>
+        <li class="youtube"><a href="https://www.youtube.com/user/programmingwithmosh" title="Mosh on YouTube" target="_blank">
+                <i class="fa fa-youtube-play"></i>
+            </a>
+        </li>
+        <li class="linkedin">
+            <a href="https://www.linkedin.com/in/mosh-hamedani/" title="Mosh on LinkedIn" target="_blank">
+                <i class="fa fa-linkedin-square"></i>
+            </a>
+        </li>
+    </ul>
+    <div style="clear: both"></div>
+</div>
